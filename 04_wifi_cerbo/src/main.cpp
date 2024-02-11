50ace95ea4e5c2a5aaa668ecbb038064c6be5b7d
NightHawk32 <nighthawk2244@gmail.com>
2024-02-11 18:34:53 +0100

    added code to test modbus TCP connection

--- 04_wifi_cerbo/src/main.cpp
+++ 04_wifi_cerbo/src/main.cpp
@@ -1,86 +1,16 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <time.h>
+#include <ModbusClientTCPasync.h>
 #include "../99_include/board.h"
 #include "../99_include/myWifi.h"
 
 bool ledState = LED_OFF;
-const long  gmtOffset_sec = 3600;
-const int   daylightOffset_sec = 3600;
+IPAddress ip = {192, 168, 0, 191};          // IP address of modbus server
+uint16_t port = 33007;                      // port of modbus server
 
-
-void printLocalTime(){
-  struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
-    Serial.println("Failed to obtain time");
-    return;
-  }
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-}
-
-void scanNetworks() {
-  Serial.println("Scan start");
-
-  // WiFi.scanNetworks will return the number of networks found.
-  int n = WiFi.scanNetworks();
-  Serial.println("Scan done");
-  if (n == 0) {
-      Serial.println("no networks found");
-  } else {
-      Serial.print(n);
-      Serial.println(" networks found");
-      Serial.println("Nr | SSID                             | RSSI | CH | Encryption");
-      for (int i = 0; i < n; ++i) {
-          // Print SSID and RSSI for each network found
-          Serial.printf("%2d",i + 1);
-          Serial.print(" | ");
-          Serial.printf("%-32.32s", WiFi.SSID(i).c_str());
-          Serial.print(" | ");
-          Serial.printf("%4ld", WiFi.RSSI(i));
-          Serial.print(" | ");
-          Serial.printf("%2ld", WiFi.channel(i));
-          Serial.print(" | ");
-          switch (WiFi.encryptionType(i))
-          {
-          case WIFI_AUTH_OPEN:
-              Serial.print("open");
-              break;
-          case WIFI_AUTH_WEP:
-              Serial.print("WEP");
-              break;
-          case WIFI_AUTH_WPA_PSK:
-              Serial.print("WPA");
-              break;
-          case WIFI_AUTH_WPA2_PSK:
-              Serial.print("WPA2");
-              break;
-          case WIFI_AUTH_WPA_WPA2_PSK:
-              Serial.print("WPA+WPA2");
-              break;
-          case WIFI_AUTH_WPA2_ENTERPRISE:
-              Serial.print("WPA2-EAP");
-              break;
-          case WIFI_AUTH_WPA3_PSK:
-              Serial.print("WPA3");
-              break;
-          case WIFI_AUTH_WPA2_WPA3_PSK:
-              Serial.print("WPA2+WPA3");
-              break;
-          case WIFI_AUTH_WAPI_PSK:
-              Serial.print("WAPI");
-              break;
-          default:
-              Serial.print("unknown");
-          }
-          Serial.println();
-          delay(10);
-      }
-  }
-  Serial.println("");
-
-  // Delete the scan result to free memory for code below.
-  WiFi.scanDelete();
-}
+// Create a ModbusTCP client instance
+ModbusClientTCPasync MB(ip, port);
 
 void setup_wifi() {
   delay(10);
@@ -93,6 +23,29 @@ void setup_wifi() {
   }
   Serial.print("WiFi connected: ");
   Serial.println(MY_WIFI_SSID);
+  IPAddress wIP = WiFi.localIP();
+  Serial.printf("WIFi IP address: %u.%u.%u.%u\n", wIP[0], wIP[1], wIP[2], wIP[3]);
+}
+
+void handleData(ModbusMessage response, uint32_t token) 
+{
+  Serial.printf("Response: serverID=%d, FC=%d, Token=%08X, length=%d:\n", response.getServerID(), response.getFunctionCode(), token, response.size());
+  for (auto& byte : response) {
+    Serial.printf("%02X ", byte);
+  }
+  Serial.println("");
+  uint16_t word1;
+  response.get(3, word1);
+  Serial.println(word1);
+}
+ 
+// Define an onError handler function to receive error responses
+// Arguments are the error code returned and a user-supplied token to identify the causing request
+void handleError(Error error, uint32_t token) 
+{
+  // ModbusError wraps the error code and provides a readable error message for it
+  ModbusError me(error);
+  Serial.printf("Error response: %02X - %s token: %d\n", (int)me, (const char *)me, token);
 }
 
 void setup() {
@@ -107,19 +60,36 @@ void setup() {
 
   Serial.println("Setup done ....");
 
-  scanNetworks();
   setup_wifi();
+
+  MB.onDataHandler(&handleData);
+// - provide onError handler function
+  MB.onErrorHandler(&handleError);
+// Set message timeout to 2000ms and interval between requests to the same host to 200ms
+  MB.setTimeout(10000);
+// Start ModbusTCP background task
+  MB.setIdleTimeout(60000);
 }
 
 void loop() {
+  static unsigned long lastMillis = 0;
+  if (millis() - lastMillis > 5000) {
+    lastMillis = millis();
+
+
+    Serial.printf("sending request with token %d\n", (uint32_t)lastMillis);
+    Error err;
+    err = MB.addRequest((uint32_t)lastMillis, 100, READ_HOLD_REGISTER, 843, 1);
+    if (err != SUCCESS) {
+      ModbusError e(err);
+      Serial.printf("Error creating request: %02X - %s\n", (int)e, (const char *)e);
+    }
+
+  }
   if(WiFi.status() != WL_CONNECTED){
     setup_wifi();
     digitalWrite(PIN_LED, LED_OFF);
   }else{
     digitalWrite(PIN_LED, LED_ON);
   }
-
-  configTime(gmtOffset_sec, daylightOffset_sec, NTP_SERVER);
-  printLocalTime();
-  delay(2000);
 }
