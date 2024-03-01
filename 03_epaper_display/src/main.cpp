0167d1666b1506c3259609e09f735e80957e3565
NightHawk32 <nighthawk2244@gmail.com>
2024-03-01 10:18:57 +0100

    added epaper test code

--- 03_epaper_display/src/main.cpp
+++ 03_epaper_display/src/main.cpp
@@ -1,21 +1,186 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#include <time.h>
+#include <GxEPD2_BW.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
 #include "../99_include/board.h"
-#include "../99_include/myWifi.h"
+#include "../99_include/GxEPD2_display_selection_new_style.h"
 
-bool ledState = LED_OFF;
+#define ICNT_MAX_TOUCH 5
+#define ICNT_ADDR 0x48
 
-void setup() {
-  pinMode(PIN_LED, OUTPUT);
-  digitalWrite(PIN_LED, ledState);
+const char HelloWorld[] = "Hello World!";
+const char HelloArduino[] = "Hello Arduino!";
+const char HelloEpaper[] = "Hello E-Paper!";
 
-  Serial.begin(115200);
-  Serial.println("Starting ....");
-
-  Serial.println("Setup done ....");
+void helloWorld()
+{
+  display.setRotation(1);
+  display.setFont(&FreeMonoBold9pt7b);
+  display.setTextColor(GxEPD_BLACK);
+  int16_t tbx, tby; uint16_t tbw, tbh;
+  display.getTextBounds(HelloWorld, 0, 0, &tbx, &tby, &tbw, &tbh);
+  // center the bounding box by transposition of the origin:
+  uint16_t x = ((display.width() - tbw) / 2) - tbx;
+  uint16_t y = ((display.height() - tbh) / 2) - tby;
+  display.setFullWindow();
+  display.firstPage();
+  do
+  {
+    display.fillScreen(GxEPD_WHITE);
+    display.setCursor(x, y);
+    display.print(HelloWorld);
+  }
+  while (display.nextPage());
 }
 
-void loop() {
-  delay(2000);
+void helloFullScreenPartialMode()
+{
+  //Serial.println("helloFullScreenPartialMode");
+  const char fullscreen[] = "full screen update";
+  const char fpm[] = "fast partial mode";
+  const char spm[] = "slow partial mode";
+  const char npm[] = "no partial mode";
+  display.setPartialWindow(0, 0, display.width(), display.height());
+  display.setRotation(1);
+  display.setFont(&FreeMonoBold9pt7b);
+  if (display.epd2.WIDTH < 104) display.setFont(0);
+  display.setTextColor(GxEPD_BLACK);
+  const char* updatemode;
+  if (display.epd2.hasFastPartialUpdate)
+  {
+    updatemode = fpm;
+  }
+  else if (display.epd2.hasPartialUpdate)
+  {
+    updatemode = spm;
+  }
+  else
+  {
+    updatemode = npm;
+  }
+  // do this outside of the loop
+  int16_t tbx, tby; uint16_t tbw, tbh;
+  // center update text
+  display.getTextBounds(fullscreen, 0, 0, &tbx, &tby, &tbw, &tbh);
+  uint16_t utx = ((display.width() - tbw) / 2) - tbx;
+  uint16_t uty = ((display.height() / 4) - tbh / 2) - tby;
+  // center update mode
+  display.getTextBounds(updatemode, 0, 0, &tbx, &tby, &tbw, &tbh);
+  uint16_t umx = ((display.width() - tbw) / 2) - tbx;
+  uint16_t umy = ((display.height() * 3 / 4) - tbh / 2) - tby;
+  // center HelloWorld
+  display.getTextBounds(HelloWorld, 0, 0, &tbx, &tby, &tbw, &tbh);
+  uint16_t hwx = ((display.width() - tbw) / 2) - tbx;
+  uint16_t hwy = ((display.height() - tbh) / 2) - tby;
+  display.firstPage();
+  do
+  {
+    display.fillScreen(GxEPD_WHITE);
+    display.setCursor(hwx, hwy);
+    display.print(HelloWorld);
+    display.setCursor(utx, uty);
+    display.print(fullscreen);
+    display.setCursor(umx, umy);
+    display.print(updatemode);
+  }
+  while (display.nextPage());
+  //Serial.println("helloFullScreenPartialMode done");
+}
+
+void helloArduino()
+{
+  //Serial.println("helloArduino");
+  display.setRotation(1);
+  display.setFont(&FreeMonoBold9pt7b);
+  if (display.epd2.WIDTH < 104) display.setFont(0);
+  display.setTextColor(display.epd2.hasColor ? GxEPD_RED : GxEPD_BLACK);
+  int16_t tbx, tby; uint16_t tbw, tbh;
+  // align with centered HelloWorld
+  display.getTextBounds(HelloWorld, 0, 0, &tbx, &tby, &tbw, &tbh);
+  uint16_t x = ((display.width() - tbw) / 2) - tbx;
+  // height might be different
+  display.getTextBounds(HelloArduino, 0, 0, &tbx, &tby, &tbw, &tbh);
+  uint16_t y = ((display.height() / 4) - tbh / 2) - tby; // y is base line!
+  // make the window big enough to cover (overwrite) descenders of previous text
+  uint16_t wh = FreeMonoBold9pt7b.yAdvance;
+  uint16_t wy = (display.height() / 4) - wh / 2;
+  display.setPartialWindow(0, wy, display.width(), wh);
+  display.firstPage();
+  do
+  {
+    display.fillScreen(GxEPD_WHITE);
+    //display.drawRect(x, y - tbh, tbw, tbh, GxEPD_BLACK);
+    display.setCursor(x, y);
+    display.print(HelloArduino);
+  }
+  while (display.nextPage());
+  delay(1000);
+  //Serial.println("helloArduino done");
+}
+
+void helloEpaper()
+{
+  //Serial.println("helloEpaper");
+  display.setRotation(1);
+  display.setFont(&FreeMonoBold9pt7b);
+  if (display.epd2.WIDTH < 104) display.setFont(0);
+  display.setTextColor(display.epd2.hasColor ? GxEPD_RED : GxEPD_BLACK);
+  int16_t tbx, tby; uint16_t tbw, tbh;
+  // align with centered HelloWorld
+  display.getTextBounds(HelloWorld, 0, 0, &tbx, &tby, &tbw, &tbh);
+  uint16_t x = ((display.width() - tbw) / 2) - tbx;
+  // height might be different
+  display.getTextBounds(HelloEpaper, 0, 0, &tbx, &tby, &tbw, &tbh);
+  uint16_t y = ((display.height() * 3 / 4) - tbh / 2) - tby; // y is base line!
+  // make the window big enough to cover (overwrite) descenders of previous text
+  uint16_t wh = FreeMonoBold9pt7b.yAdvance;
+  uint16_t wy = (display.height() * 3 / 4) - wh / 2;
+  display.setPartialWindow(0, wy, display.width(), wh);
+  display.firstPage();
+  do
+  {
+    display.fillScreen(GxEPD_WHITE);
+    display.setCursor(x, y);
+    display.print(HelloEpaper);
+  }
+  while (display.nextPage());
+  //Serial.println("helloEpaper done");
+}
+
+
+void setup() {
+	pinMode(PIN_LED, OUTPUT);
+	digitalWrite(PIN_LED, LED_ON);
+
+	pinMode(PIN_TOUCH_RESET, OUTPUT);
+	pinMode(PIN_TOUCH_INT, INPUT);
+	//attachInterrupt(PIN_TOUCH_INT, touchInt, FALLING);
+
+	Serial.println("Starting ....");
+	display.init(115200, true, 2, false); // USE THIS for Waveshare boards with "clever" reset circuit, 2ms reset pulse
+	if (display.pages() > 1)
+	{
+		delay(100);
+		Serial.print("pages = "); Serial.print(display.pages()); Serial.print(" page height = "); Serial.println(display.pageHeight());
+		delay(1000);
+	}
+	// first update should be full refresh
+	helloWorld();
+	delay(1000);
+	// partial refresh mode can be used to full screen,
+	// effective if display panel hasFastPartialUpdate
+	helloFullScreenPartialMode();
+	delay(1000);
+	//stripeTest(); return; // GDEH029Z13 issue
+	helloArduino();
+	delay(1000);
+	helloEpaper();
+	delay(1000);
+  	display.hibernate();
+	Serial.println("Setup done ....");
+
+}
+
+void loop()
+{	
+
 }
--- 99_include/board.h
+++ 99_include/board.h
@@ -7,6 +7,13 @@
 
 #define PIN_BOOT 9
 
+#define PIN_EPD_BUSY D3
+#define PIN_EPD_RST D2
+#define PIN_EPD_DC D1
+#define PIN_EPD_CS D0
+#define PIN_EPD_CLK D8
+#define PIN_EPD_MOSI D10
+
 #define PIN_SDA D4
 #define PIN_SCL D5
 #define PIN_TOUCH_INT D6
